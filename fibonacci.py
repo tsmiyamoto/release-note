63211c7a812c8e94b47d95867aa0e59d0aea6de6
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2023-04-25 21:44:25 +0900

    format: blackによるフォーマット

--- fibonacci.py
+++ fibonacci.py
@@ -3,16 +3,18 @@ def fib(n):
     if n < 0:
         print("n must be greater than 0")
         return
-    
+
     a, b = 0, 1
     while a < n:
-        print(a, end=' ')
+        print(a, end=" ")
         # a, b = b, a+b
         t = b
         b = a + b
         a = t
     print()
 
+
 if __name__ == "__main__":
     import sys
+
     fib(int(sys.argv[1]))
